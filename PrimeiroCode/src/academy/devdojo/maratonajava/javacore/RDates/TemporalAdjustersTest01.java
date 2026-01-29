9bd6121f755e1adcc4afc0d5f81a4a427ef61e71
James Candido <james.candidof@gmail.com>
2026-01-29 17:15:45 -0300

    lessons Modify Datas

---
+++ PrimeiroCode/src/academy/devdojo/maratonajava/javacore/RDates/TemporalAdjustersTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.RDates;
+
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        //O TemporalAdjusters realiza a mudança de datas sem precisar rolar o calendário
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+    }
+}
---
+++ PrimeiroCode/src/academy/devdojo/maratonajava/javacore/RDates/TemporalAdjustersTest02.java
@@ -0,0 +1,50 @@
+package academy.devdojo.maratonajava.javacore.RDates;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+class ObterProximoDiaUtil implements TemporalAdjuster{
+
+    @Override
+    public Temporal adjustInto(Temporal temporal){
+        DayOfWeek dayOfWeek = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+
+        int addDays;
+        switch (dayOfWeek) {
+            case FRIDAY: addDays = 3; 
+            break;
+            case SATURDAY: addDays = 2; 
+            break;
+
+            default:
+                addDays = 1;
+        }
+
+
+        return temporal.plus(addDays, ChronoUnit.DAYS);
+    }
+}
+
+public class TemporalAdjustersTest02 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        System.out.println(now);
+        System.out.println("TODAY IS " + now.getDayOfWeek());
+
+        System.out.println();
+        now = LocalDate.now().with(new ObterProximoDiaUtil());
+        System.out.println(now);
+        System.out.println("NEXT BUSINESS DAY IS " + now.getDayOfWeek());
+
+        System.out.println();
+        System.out.println("ALTERADA A DATA");
+        now = LocalDate.now().withDayOfMonth(30).with(new ObterProximoDiaUtil());
+        System.out.println(now);
+        System.out.println("NEXT BUSINESS DAY IS " + now.getDayOfWeek());
+    }
+}
