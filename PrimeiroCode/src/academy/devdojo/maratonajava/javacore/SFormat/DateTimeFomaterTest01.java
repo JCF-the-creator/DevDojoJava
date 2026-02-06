79ec870b08f058b50c35b13d3ba2249a0ca407d2
James Candido <james.candidof@gmail.com>
2026-02-06 17:46:32 -0300

    End of Dates Leassons
    DateTimeFormater

---
+++ PrimeiroCode/src/academy/devdojo/maratonajava/javacore/SFormat/DateTimeFomaterTest01.java
@@ -0,0 +1,48 @@
+package academy.devdojo.maratonajava.javacore.SFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateTimeFomaterTest01 {
+    public static void main(String[] args) {
+        //Format transforma do objeto para uma String
+        //Parse transforma da String em um Objato
+        LocalDate date = LocalDate.now();
+        String s1 = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String s2 = date.format(DateTimeFormatter.ISO_DATE);
+        String s3 = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println();
+        LocalDate parse1 = LocalDate.parse("20210219", DateTimeFormatter.BASIC_ISO_DATE); // se o texto está formatado no padrão ISO o java não sabe do padrão utilizado até você informar pelo DateTimeFormater o padrão da String
+        LocalDate parse2 = LocalDate.parse("2021-02-19+05:00", DateTimeFormatter.ISO_DATE);
+        LocalDate parse3 = LocalDate.parse("2021-02-19", DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(parse1);
+        System.out.println(parse2);
+        System.out.println(parse3);
+
+        LocalDateTime now = LocalDateTime.now();
+        String s4 = now.format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(s4);
+        LocalDateTime parse4 = LocalDateTime.parse("2021-02-19T18:46:04.1121294", DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(parse4);
+        //dd/MM/yyyy
+        //MM/dd/yyyy
+        //yyyy/MM/dd
+
+        DateTimeFormatter formatterBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formatBR = LocalDate.now().format(formatterBR);
+        System.out.println(formatBR);
+        LocalDate parseBR = LocalDate.parse("19/02/2021", formatterBR);
+        System.out.println(parseBR);
+
+        DateTimeFormatter formatterGR = DateTimeFormatter.ofPattern("dd.MMMM.yyyy", Locale.GERMAN);
+        String formatGR = LocalDate.now().format(formatterGR);
+        System.out.println(formatGR);
+        LocalDate parseGR = LocalDate.parse("19.Februar.2021", formatterGR);
+        System.out.println(parseGR);
+    }
+}
