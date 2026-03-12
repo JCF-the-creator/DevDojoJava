e30c09c4c41a2ce38ebda4b424e96505fd6d23da
James Candido <james.candidof@gmail.com>
2026-03-12 11:39:11 -0300

    Serialization Lesson

---
+++ PrimeiroCode/src/academy/devdojo/maratonajava/javacore/XSerialization/Dominio/Aluno.java
@@ -0,0 +1,47 @@
+package academy.devdojo.maratonajava.javacore.XSerialization.Dominio;
+
+import java.io.Serializable;
+
+public class Aluno implements Serializable{
+    private Long id;
+    private String login;
+    private String password;
+
+    
+    public Aluno(Long id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Aluno [id=" + id + ", login=" + login + ", password=" + password + "]";
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+
+}
---
+++ PrimeiroCode/src/academy/devdojo/maratonajava/javacore/XSerialization/test/SerializationTest01.java
@@ -0,0 +1,40 @@
+package academy.devdojo.maratonajava.javacore.XSerialization.test;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import academy.devdojo.maratonajava.javacore.XSerialization.Dominio.Aluno;
+
+public class SerializationTest01 {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno(1L, "James", "8342");
+        //serializar(aluno);
+        deserializar(); //não executa o construtor
+    }
+    
+    //ao Serializar vc transforma as informações em um Array de bits trabalhando assim em baixo nivel 
+    private static void serializar(Aluno aluno){
+        Path nomeArquivo = Paths.get("pasta/aluno.ser");
+        try(ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(nomeArquivo))){ 
+            oos.writeObject(aluno);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void deserializar(){
+        Path nomeArquivo = Paths.get("pasta/aluno.ser");
+        try(ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(nomeArquivo))){ 
+
+            Aluno aluno = (Aluno) ois.readObject();
+            System.out.println(aluno);
+        }catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+}
